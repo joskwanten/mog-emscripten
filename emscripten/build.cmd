694b05e9f8d9c705f2d5ddd481eb621dd95674ac
Jos <jos.kwanten@gmail.com>
2019-05-30 18:38:33 +0200

    Added assets folder

---
+++ emscripten/build.cmd
@@ -0,0 +1,26 @@
+
+del *.o
+call emcc ..\sources\bitmaps.cpp -c
+call emcc ..\sources\debug-report.cpp -c
+call emcc ..\sources\debug.cpp -c
+call emcc ..\sources\demons.cpp -c
+call emcc ..\sources\drawing.cpp -c
+call emcc ..\sources\filehandling.cpp -c
+call emcc ..\sources\gameaux.cpp -c
+call emcc ..\sources\gamecycle.cpp -c
+call emcc ..\sources\gameinteractions.cpp -c
+call emcc ..\sources\gameobjects.cpp -c
+call emcc ..\sources\gametiles.cpp -c
+call emcc ..\sources\ingamecycle.cpp -c
+call emcc ..\sources\keyboard.cpp -c
+call emcc ..\sources\line_clip.cpp -c
+call emcc ..\sources\loadpcx.cpp -c
+call emcc ..\sources\main.cpp -c
+call emcc ..\sources\passwords.cpp -c
+call emcc ..\sources\path.cpp -c
+call emcc ..\sources\render.cpp -c
+call emcc ..\sources\roomchange.cpp -c
+call emcc ..\sources\shrines.cpp -c
+call emcc ..\sources\sound.cpp -c
+call emcc bitmaps.o debug-report.o debug.o demons.o drawing.o filehandling.o gameaux.o gamecycle.o gameinteractions.o gameobjects.o gametiles.o ingamecycle.o keyboard.o line_clip.o loadpcx.o main.o passwords.o path.o render.o roomchange.o shrines.o sound.o -o mog.html --preload-file ../assets@/ -s TOTAL_MEMORY=67108864 --use-preload-plugins -s STB_IMAGE=1 -O2
+
